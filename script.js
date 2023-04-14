38e8db2b5cf43e8591798c02d8deebaf2be949a5
bigsmart-cpu <126834635+bigsmart-cpu@users.noreply.github.com>
2023-04-14 10:13:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.write("Hello, world!");
+function sayHello() {
+  let response = prompt("What is your name?");
+  alert("Hello " + response + ", have a nice day!");
+}
+function add(a, b) {
+  return a + b;}
+console.log(add(20, 30));
+console.log(add(200, 300));
+function showTopic() {
+  let x = document.getElementById('demo');
+  x.style.fontSize = "25px"
+  x.style.color = "red"
+}
